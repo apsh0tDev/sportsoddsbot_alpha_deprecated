3cd49d3404d3b629251beccb16585e694c42461a
apsh0tDev <apsh0t@proton.me>
2024-09-03 18:22:06 -0400

    WIP and sync

--- live_runners.py
+++ live_runners.py
@@ -1,6 +1,9 @@
-from db import db
+import pytz
 import asyncio
 import scores365
+from db import db
+from datetime import datetime, timedelta
+from dateutil.parser import parse
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 scheduler = AsyncIOScheduler()
@@ -20,8 +23,27 @@ def sleep_bot():
             if job.name == "get_scores":
                 scheduler.remove_job(job_id=job.id)
     
-        print(jobs_names)           
-            
+        print(jobs_names)
+
+def wake_up_bot():
+    print("RUNNING WAKE UP ⛅ BOT FUNCTION")
+    table = db.table("schedule").select("*").execute()
+    dates = [parse(item['date']) for item in table.data]
+    ny_tz = pytz.timezone("America/New_York")
+    now_ny = datetime.now(ny_tz)
+    dates_ny = [date.astimezone(ny_tz) for date in dates]
+    closest_date_ny = min(dates_ny, key=lambda date: abs(date - now_ny))
+    closest_date_str = closest_date_ny.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+
+    with open('next_game.txt', 'w') as file:
+        file.write(f"The closest game is scheduled for: {closest_date_str}")
+
+    jobs = scheduler.get_jobs()
+    jobs_names = [job.name for job in jobs]
+    if 'get_scores' not in jobs_names:
+        run_time = closest_date_ny - timedelta(minutes=5)
+        scheduler.add_job(get_scores, 'date', run_date=run_time)
+
 
 def live():
     jobs = scheduler.get_jobs()
@@ -36,9 +58,11 @@ job_defaults = {
 
 scheduler.configure(job_defaults=job_defaults)
 scheduler.add_job(sleep_bot, 'interval', minutes=2)
+scheduler.add_job(wake_up_bot, 'interval', minutes=2)
 scheduler.start()
 
 try:
+    wake_up_bot()
     live()
     asyncio.get_event_loop().run_forever()
 except (KeyboardInterrupt, SystemExit):
